b5831331f0a367b1ffb56fea61c81466e297cba5
Peter Flanigan <pjfl@cpan.org>
2008-06-24 15:50:14 +0000

    Import of PJFL/Data-Validation-0.1.30 from CPAN.
    gitpan-cpan-distribution: Data-Validation
    gitpan-cpan-version:      0.1.30
    gitpan-cpan-path:         PJFL/data-validation/Data-Validation-0.1.30.tar.gz
    gitpan-cpan-author:       PJFL
    gitpan-cpan-maturity:     released

--- lib/Data/Validation.pm
+++ lib/Data/Validation.pm
@@ -1,6 +1,6 @@
 package Data::Validation;
 
-# @(#)$Id: Validation.pm 26 2008-06-09 09:18:48Z pjf $
+# @(#)$Id: Validation.pm 30 2008-06-24 15:29:30Z pjf $
 
 use strict;
 use warnings;
@@ -12,7 +12,7 @@ use Readonly;
 use Regexp::Common qw(number);
 use Scalar::Util   qw(looks_like_number);
 
-use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 26 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.1.%d', q$Rev: 30 $ =~ /\d+/gmx );
 
 Readonly my %ATTRS => ( max_length => undef,
                         max_value  => undef,
@@ -235,7 +235,7 @@ Data::Validation - Check data values form conformance with constraints
 
 =head1 Version
 
-0.1.$Rev: 26 $
+0.1.$Rev: 30 $
 
 =head1 Synopsis
 
@@ -511,4 +511,3 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
 # mode: perl
 # tab-width: 3
 # End:
-
